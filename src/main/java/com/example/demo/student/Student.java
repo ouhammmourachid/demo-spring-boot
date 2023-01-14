be1d04ea14c6c98211f91f89c795207196226d73
rachid ouhammou <rachidouhammou21@gmail.com>
2023-01-14 01:07:30 +0100

    handle all the tpe of request for the student class

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,47 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "student")
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_name",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    @Transient
+    private Integer age;
+
+    public Student(String name,
+                   String email,
+                   LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob,LocalDate.now()).getYears();
+    }
+}
