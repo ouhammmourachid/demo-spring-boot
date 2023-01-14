089c018b629dd46ac472cd128bf5378b93d7a3b5
rachid ouhammou <rachidouhammou21@gmail.com>
2023-01-14 15:13:00 +0100

    complite the delete request

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
-    //@Query("SELECT s from student s WHERE s.email = ?1")
+    @Query("SELECT s from student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String Email);
 }
